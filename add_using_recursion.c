4c4db2fd6480d276d9f67a23f36ec57fe0b32dd4
parthm2310 <86843036+parthm2310@users.noreply.github.com>
2021-07-19 19:04:39 +0530

    Add files via upload

---
+++ add_using_recursion.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+addition();
+main()
+{
+	addition();
+}
+addition()
+{
+	int no1,no2;
+	printf("input two numbers :");
+	scanf("%d%d",&no1,&no2);
+	printf("sum of digits :%d",no1+no2); 
+}
