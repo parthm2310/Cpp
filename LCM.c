34b5793fa83c6437bd3442fc106873bf25df81a0
parthm2310 <86843036+parthm2310@users.noreply.github.com>
2021-07-19 22:11:50 +0530

    Add files via upload

---
+++ LCM.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int lcm(int a,int b);
+int main()
+{
+	int no1,no2;
+	printf("Enter the first number:");
+	scanf("%d",&no1);
+	printf("Enter the second number:");
+	scanf("%d",&no2);
+	printf("LCM of these two numbers is:%d",lcm(no1,no2));
+}	
+	
+int lcm(int a,int b)
+{
+	static int check;
+	check=check+a;
+		if( check % a == 0 && check % b == 0 )
+		return check;
+	return lcm(a,b); 
+}
