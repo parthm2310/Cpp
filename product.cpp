eeb556fcfbf78325b7ca0a13ad64df4197f2b209
parthm2310 <86843036+parthm2310@users.noreply.github.com>
2021-08-06 13:36:38 +0530

    Add files via upload

---
+++ product.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	int product;
+	cout<<"Enter two numbers :\n";
+	cin>>a;
+	cin>>b;
+	product=a*b;
+	cout<<"The product is : ";
+	cout<<product;
+}
