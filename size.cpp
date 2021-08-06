c2daf7122f4c262e41ad5203c9cd4d08d578c167
parthm2310 <86843036+parthm2310@users.noreply.github.com>
2021-08-06 13:13:06 +0530

    Add files via upload

---
+++ size.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"The size of char is : "<<sizeof(char)<<" byte\n";
+	cout<<"The size of int is : "<<sizeof(int)<<" bytes\n";
+	cout<<"The size of float is : "<<sizeof(float)<<" bytes\n";
+	cout<<"The size of double is : "<<sizeof(double)<<" bytes\n";
+}
