fa3dc40b1bd44cee3cd995886237141c72fa6e71
parthm2310 <86843036+parthm2310@users.noreply.github.com>
2021-08-10 14:51:33 +0530

    Add files via upload

---
+++ score.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	cout<<"Enter your score (out of 100) :\n";
+	cin>>a;
+	if(a>=0 && a<100)
+	{
+		if(a<=40)
+		{
+			cout<<"Fail!";
+		}
+		else
+		{
+			if(a<70)
+			{
+				cout<<"Grade : D";
+			}
+			else
+			{
+				if(a<80)
+				{
+					cout<<"Grade : C";
+				}
+				else
+				{
+					if(a<90)
+					{
+						cout<<"Grade : B";	
+					}
+					else
+					{
+						cout<<"Grade : A";	
+					}
+				}
+			}
+		}
+	}
+	else
+	{
+	cout<<"invalid input!";
+	}
+}
