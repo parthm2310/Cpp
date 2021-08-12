3158daea5e76b9c8fbdb739a765856f5ba54e1b9
parthm2310 <86843036+parthm2310@users.noreply.github.com>
2021-08-12 22:36:23 +0530

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i[4];
+	cout<<"Enter elements : \n";
+	cin>>i[0]>>i[1]>>i[2]>>i[3]>>i[4];
+	cout<<"You entered : "<<i[0]<<" "<<i[1]<<" "<<i[2]<<" "<<i[3]<<" "<<i[4];
+}
