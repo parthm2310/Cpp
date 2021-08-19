435509633c613cc2ee9884186a494dabc8944beb
parthm2310 <86843036+parthm2310@users.noreply.github.com>
2021-08-19 21:21:09 +0530

    Update sum2prime.cpp

--- sum2prime.cpp
+++ sum2prime.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 int func(int num) {
    int i;
-   int flag = 1;
+   int re = 1;
    for(i = 2; i <= num/2; ++i) {
       if(num % i == 0) {
-         flag = 0;
+         re = 0;
          break;
       }
    }
-   return flag;
+   return re;
 }
 int main() {
    int num , i;
